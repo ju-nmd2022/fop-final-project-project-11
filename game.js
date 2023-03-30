371eba9040e2a02629d08a71155e203bb76ae3c0
Liden119 <117270216+Liden119@users.noreply.github.com>
2023-03-30 18:55:37 +0200

    moving objects and screen

--- game.js
+++ game.js
@@ -5,9 +5,40 @@ let asteroidGray = [150, 150, 150];
 let shipBackgroundColor = [150, 170, 190];
 
 let asteroidHP = 100;
-let asteroidX = 200;
-let asteroidY = 200;
+let obstacleRotation = 0;
 
+//Variables to move obstacles positions when using "arrows".
+let movementX = 0;
+let movementY = 0;
+let movementSpeed = 15;
+let positionTrackerX = 0;
+let positionTrackerY = 0;
+
+//Stars
+let stars = [];
+for (let i = 0; i < 2000; i++) {
+  const star = {
+    x: Math.floor(Math.random() * (width + 600) - 300),
+    y: Math.floor(Math.random() * (height + 500) - 250),
+  };
+  stars.push(star);
+}
+
+//Obstacles
+let obstacles = [];
+for (let i = 0; i < 6; i++) {
+  const obstacle = {
+    x: Math.floor(Math.random() * 100 - 400),
+    y: Math.floor(Math.random() * height),
+    hp: Math.floor(Math.random() * 100) + 100,
+    scale: Math.random() * 0.2 + 0.1,
+    velocityX: Math.random() * 3 + 2,
+    velocityY: Math.random() * 1 - 1,
+  };
+  obstacles.push(obstacle);
+}
+
+let obstacleVelocity = Math.random() * 5 - 2.5;
 // Aim Crosshair
 function aim(x, y) {
   push();
@@ -64,30 +95,78 @@ function asteroid(xAsteroid, yAsteroid, scaleAsteroid, rotationAsteroid) {
 
 function shipBackground() {
   fill(shipBackgroundColor);
-  rect(0, height - 160, width, 160);
-  triangle(0, 0, 80, height - 160, 0, height);
-  triangle(width, 0, width - 80, height - 160, width, height);
+  rect(0, height - 140, width, 140);
+  triangle(0, 0, 60, height - 140, 0, height);
+  triangle(width, 0, width - 60, height - 140, width, height);
 }
 
 function draw() {
+  //Spacebackground with stars
   background(black);
+  for (let star of stars) {
+    fill(white);
+    ellipse(star.x + movementX, star.y + movementY, 2);
+  }
 
-  if (asteroidHP > 0) {
-    asteroid(asteroidX, asteroidY, 0.2, 0);
+  for (let obstacle of obstacles) {
+    if (
+      obstacle.hp > 0 &&
+      obstacle.y > -280 &&
+      obstacle.y < width + 280 &&
+      obstacle.x < width + 280
+    ) {
+      asteroid(
+        obstacle.x + movementX,
+        obstacle.y + movementY,
+        obstacle.scale,
+        obstacleRotation
+      );
+      obstacle.x = obstacle.x + obstacle.velocityX;
+      obstacle.y = obstacle.y + obstacle.velocityY;
+    }
+  }
+
+  //Moves screen when using arrows
+  if (
+    (positionTrackerX < 300 && keyIsDown(65)) ||
+    (positionTrackerX < 300 && keyIsDown(37))
+  ) {
+    movementX = movementX + movementSpeed;
+    positionTrackerX = positionTrackerX + movementSpeed;
+  }
+  if (
+    (positionTrackerX > -300 && keyIsDown(68)) ||
+    (positionTrackerX > -300 && keyIsDown(39))
+  ) {
+    movementX = movementX - movementSpeed;
+    positionTrackerX = positionTrackerX - movementSpeed;
+  }
+  if (
+    (positionTrackerY < 250 && keyIsDown(87)) ||
+    (positionTrackerY < 250 && keyIsDown(38))
+  ) {
+    movementY = movementY + movementSpeed;
+    positionTrackerY = positionTrackerY + movementSpeed;
+  }
+  if (
+    (positionTrackerY > -250 && keyIsDown(83)) ||
+    (positionTrackerY > -250 && keyIsDown(40))
+  ) {
+    movementY = movementY - movementSpeed;
+    positionTrackerY = positionTrackerY - movementSpeed;
   }
 
   aim(mouseX, mouseY);
-
   shipBackground();
 }
 
-function mouseClicked() {
-  if (
-    mouseX > asteroidX - 30 &&
-    mouseX < asteroidX + 30 &&
-    mouseY > asteroidY - 27 &&
-    mouseY < asteroidY + 25
-  ) {
-    asteroidHP = asteroidHP - 20;
-  }
-}
+// function mouseClicked() {
+//   if (
+//     mouseX > obstacle.x - 30 &&
+//     mouseX < obstacle.x + 30 &&
+//     mouseY > obstacle.y - 27 &&
+//     mouseY < obstacle.y + 25
+//   ) {
+//     obstacle.hp = obstacle.hp - 20;
+//   }
+// }
