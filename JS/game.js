395936db4f7145a2cd2dd0d6dc24ad2b50591574
Liden119 <danielliden2@hotmail.com>
2023-05-25 10:55:44 +0200

    new teest

--- JS/game.js
+++ JS/game.js
@@ -20,6 +20,7 @@ innerWidth = innerWidth - 50;
 //Canvas setup
 function setup() {
   const canvas = createCanvas(innerWidth, innerHeight);
+  canvas.parent("midScreen");
 }
 window.setup = setup;
 
